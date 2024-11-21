4204d6e18230fe2955f811ff8cb6a4c9a50098df
BiGPiG <Bogdan.ars.15@yandex.ru>
2024-11-21 22:36:58 +0300

    fixed move funcs

--- Player.h
+++ Player.h
@@ -12,13 +12,19 @@ private:
     float yVelocity = 0;
     float gravity = 0.5;
     float jumpHeight = 15;
+
     Vector2 velocity = {5, 0};
+
     int x;
     int y;
     Rectangle hitBox;
     Rectangle textureRec;
-    bool isJumping = true;
+    bool isJumping = false;
     bool isMoving = false;
+    bool leftRun = false;
+    bool rightRun = false;
+    bool fromLadder = false;
+    bool fromGravity = false;
     Texture2D playerTexture;
     Vector2 position;
 
@@ -31,6 +37,14 @@ public:
         hitBox = { 25 + startX, startY, 50.0f, 128.0f }; 
         textureRec = { 0, 0, 101.0f, 128.0f };
         playerTexture = LoadTexture(texturePath); // Загрузка текстуры игрока
+    }
+
+    void setLeftRun(bool flag) {
+        leftRun = flag;
+    }
+
+    void setRightRun(bool flag) {
+        rightRun = flag;
     }
 
     int getWidth() {
@@ -69,17 +83,17 @@ public:
         UnloadTexture(playerTexture);
     }
 
-    bool isOnGround(std::vector<std::shared_ptr<MapObject>> mapObjects) {
+    int isOnGround(std::vector<std::shared_ptr<MapObject>> mapObjects) {
         for (auto obj: mapObjects) {
              if (auto platform = dynamic_cast<Platform*>(obj.get()))  {
                 
                 if (platform->isCollision(hitBox)) {
-                    std::cout << "platform" << std::endl;
-                    return true;
+                    //std::cout << "platform" << std::endl;
+                    return platform->getRectangle().y;
                 }
             } 
         }
-        return false;
+        return 0;
     }
 
     bool isOnLadder(std::vector<std::shared_ptr<MapObject>> mapObjects) {
@@ -88,7 +102,7 @@ public:
                 Vector2 hitBoxCenter = { hitBox.x + hitBox.width, hitBox.y + hitBox.height};
                 
                 if (ladder->isCollision(hitBoxCenter)) {
-                    std::cout << "ladder" << std::endl;
+                    //std::cout << "ladder" << std::endl;
                     return true;
                 }
             }
@@ -97,15 +111,36 @@ public:
     }
 
     void runLeft(const std::vector<std::shared_ptr<MapObject>> mapObjects) {
-        position.x -= velocity.x;
-        hitBox.x = 25 + position.x;
-        /*
-        if (isOnLadder(mapObjects)) {
-            isJumping = false;
-            position.y += 0.77; // Смещение по Y при нахождении на лестнице
+
+        std::cout << "runl" << std::endl;
+
+        leftRun = true;
+        rightRun = false;
+
+        if (isOnGround(mapObjects) && !isOnLadder(mapObjects)) {
+            
+            position.x -= velocity.x;
+            hitBox.x = 25 + position.x;
+            
+            position.y = isOnGround(mapObjects) - playerTexture.height / 2;
+            hitBox.y = position.y;
+           
+        } else if (isOnLadder(mapObjects)) {
+
+            position.x -= velocity.x;
+            hitBox.x = 25 + position.x;
+            position.y += 2.5; // Смещение по Y при нахождении на лестнице
             hitBox.y = position.y; // Обновляем хитбокс
+        } else if (isJumping) {
+            position.x -= velocity.x;
+            hitBox.x = 25 + position.x;
+            position.y += gravity;
+            hitBox.y = position.y;
+        } else {
+            position.y += gravity;
+            hitBox.y = position.y;
         }
-        */
+
         if (textureRec.width > 0) {
             textureRec.width = -textureRec.width;
         }
@@ -113,74 +148,121 @@ public:
 
     void runRight(const std::vector<std::shared_ptr<MapObject>> mapObjects) {
 
-        position.x += velocity.x;
-        hitBox.x = 25 + position.x;
-        /*
-        if (isOnLadder(mapObjects)) {
-            isJumping = false;
-            position.y -= 0.77; // Смещение по Y при нахождении на лестнице
+        std::cout << "runr" << std::endl;
+
+        rightRun = true;
+        leftRun = false;
+        
+        if (isOnGround(mapObjects) && !isOnLadder(mapObjects)) {
+            position.x += velocity.x;
+            hitBox.x = 25 + position.x;
+
+            position.y = isOnGround(mapObjects) - playerTexture.height / 2;
+            hitBox.y = position.y;
+
+        } else if (isOnLadder(mapObjects)) {
+
+            position.x += velocity.x;
+            hitBox.x = 25 + position.x;
+            position.y -= 2.5; // Смещение по Y при нахождении на лестнице
             hitBox.y = position.y; // Обновляем хитбокс
+
+        } else if (isJumping) {
+
+            position.x += velocity.x;
+            hitBox.x = 25 + position.x;
+            position.y += gravity;
+            hitBox.y = position.y;
+
+        } else {
+
+            position.y += gravity;
+            hitBox.y = position.y;
+
         }
-        */
+
         if (textureRec.width < 0) {
             textureRec.width = -textureRec.width;
         }
     }
 
     void jump(const std::vector<std::shared_ptr<MapObject>> mapObjects) {
-        if (!isJumping || isOnLadder(mapObjects)) { // Проверка, может ли игрок прыгнуть
+        if (!isJumping && (isOnGround(mapObjects) || isOnLadder(mapObjects))) { // Проверка, может ли игрок прыгнуть
+            std::cout << "jump" << std::endl;
             velocity.y = -jumpHeight; // Устанавливаем вертикальную скорость для прыжка
             isJumping = true;   // Устанавливаем флаг прыжка
         }
     }
 
-    void update(const std::vector<std::shared_ptr<MapObject>> mapObjects, int currentFrame) {
+    void updateAnimation(int &currentFrame) {
 
-        if (isOnLadder(mapObjects) && !isOnGround(mapObjects)) {
-            isJumping = false; // Сбрасываем флаг прыжка при нахождении на лестнице
-            
-            // Автоматическое движение по лестнице
-            if (IsKeyDown(KEY_RIGHT)) { // Движение вправо
-                position.y -= 0.77; // Смещение вверх по лестнице
-            } 
-            if (IsKeyDown(KEY_LEFT)) { // Движение влево
-                position.y += 0.77; // Смещение вниз по лестнице
+        if (isJumping) {
+            textureRec.x = (float)playerTexture.width / countFrameRun * currentFrame;
+            textureRec.y = (float)playerTexture.height / 2; // Кадр прыжка
+        } else if (isMoving) {
+            textureRec.x = (float)playerTexture.width / countFrameRun * currentFrame;
+            textureRec.y = 0; // Кадр бега
+        } else {
+            currentFrame = 0; 
+            textureRec.x = 0; // Возвращаемся к первому кадру
+            textureRec.y = 0; 
+        }
+    }
+
+    void updatePhysics(const std::vector<std::shared_ptr<MapObject>> &mapObjects) {
+        
+        if (isJumping) {
+            if (isMoving) {
+                std::cout << "runExtra" << std::endl;
+                /*
+                if (leftRun) {
+                    position.x -= velocity.x; // Move left
+                }
+                if (rightRun) {
+                    position.x += velocity.x; // Move right
+                }
+                */
+            }
+            std::cout << "jumpJump" << std::endl;
+            velocity.y += gravity; // Применяем гравитацию к вертикальной скорости
+            position.y += velocity.y; // Обновляем позицию по Y
+            hitBox.y = position.y;
+
+            if (isOnGround(mapObjects)) { // Проверка на приземление
+                std::cout << "notJumpJump" << std::endl;
+                position.y = isOnGround(mapObjects) - playerTexture.height / 2; // Сброс позиции на землю
+                isJumping = false; // Завершение прыжка
+                velocity.y = 0.0f; // Сброс скорости прыжка
+                hitBox.y = position.y;
+            } else if (isOnLadder(mapObjects)) {
+                // Если игрок на лестнице, корректируем его позицию
+                position.y -= 2.5; // Поднимаем игрока немного выше, чтобы он не проваливался сквозь лестницу
+                hitBox.y = position.y; // Обновляем хитбокс
+                isJumping = false; 
+                velocity.y = 0.0f;
+            }
+        } else if (!isOnGround(mapObjects)) {
+            if (!isOnLadder(mapObjects)) {
+                velocity.y += gravity; // Если не на земле, применяем гравитацию
+                position.y += velocity.y;
+                hitBox.y = position.y;
             }
 
-            hitBox.y = position.y; // Обновляем хитбокс
-            hitBox.x = position.x + 25; // Обновляем хитбокс с учетом смещения
-
-            if (IsKeyPressed(KEY_SPACE)) { // Прыжок при нажатии пробела
-                jump(mapObjects);
+            if (isOnGround(mapObjects)) { 
+                position.y = isOnGround(mapObjects) - playerTexture.height / 2;
+                isJumping = false; 
+                velocity.y = 0.0f;
+            } else if (isOnLadder(mapObjects)) {
+                isJumping = false; 
+                velocity.y = 0.0f;
             }
             
         } else {
-
-            if (isJumping) {
-
-                velocity.y += gravity; // Применяем гравитацию к вертикальной скорости
-
-                position.y += velocity.y;   // Обновляем позицию hitbox
-                hitBox.y = position.y;
-                
-                if (isOnGround(mapObjects) || isOnLadder(mapObjects)) { // Проверяем, на земле ли игрок
-                    isJumping = false;      // Если да, сбрасываем флаг прыжка
-                    velocity.y = 0;          // Сбрасываем вертикальную скорость
-                    position.y = (int)(hitBox.y);    // Обновляем позицию Y до уровня платформы
-                }
-            } else {
-                if (!(isOnGround(mapObjects) || isOnLadder(mapObjects))) {
-                    jump(mapObjects);          // Если на земле, сбрасываем вертикальную скорость
-                } else {
-                    velocity.y = 0;               // Если не на земле и не в прыжке, начинаем падение
-                }
-            }
-
+            position.y = isOnGround(mapObjects) - playerTexture.height / 2;
+            velocity.y = 0.0f; // Если игрок на земле, сбрасываем вертикальную скорость
         }
         
-        textureRec.x = (float) playerTexture.width / countFrameRun * currentFrame; 
-        textureRec.y = isJumping ? 128 : 0;
-        
+        hitBox.x = position.x + 25; // Обновление хитбокса по X
     }
 
     void draw() const {
